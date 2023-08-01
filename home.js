4581dab0f3992cebb7cef0794aedd73f2b3756e3
SnehaFSD <sinegashaan@gmail.com>
2023-08-01 23:09:49 +0530

    update

---
+++ home.js
@@ -0,0 +1,35 @@
+// //Normal function
+const a = "hello how are you";
+//const c = a.toString();
+function caps(a) {
+    // let b = a[1].toUpperCase();
+    // return b;
+    let b = a[0].toUpperCase() + a.slice(1);
+    return b;
+}
+console.log(caps(a));
+
+//Anonyomus Function
+var a = "hello how are you";
+var caps = function(a)
+{
+    let b = a[0].toUpperCase() + a.slice(1);
+    return b;
+}
+console.log(caps(a));
+
+
+//IIFE (Immediately Invoked Function Expression)
+  //Example :-Print the odd numbers in an array
+(function caps(a){
+    var b = a[0].toUpperCase() + a.slice(1);
+    console.log(b);
+})("hello how are you")
+
+//Arrow Function (ES6)
+var a = "hello how are u";
+var caps = (a) => {
+    let b = a[0].toUpperCase() + a.slice(1);
+    return b;
+};
+console.log(caps(a));
